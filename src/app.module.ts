e98a9ae3961018eb594cafdc50d899e2ecf0349c
Young <43630508+young-sinf@users.noreply.github.com>
2022-06-01 09:04:59 +0900

    chore: Add typeORM (#4)
    * feat: Add typeorm
    
    * chore: Add typeorm settings
    
    * fix: Fix typeorm config
    
    * chore: Remove unnessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
+    TypeOrmModule.forRoot(),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
