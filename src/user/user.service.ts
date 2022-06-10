038f53a4a0af1f7334268f55a1dea2909ac38212
young-sinf <wjdtp93@gmail.com>
2022-06-10 11:47:55 +0900

    feat: Add storing user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/@types/users';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 
@@ -9,4 +10,22 @@ export class UserService {
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
   ) {}
+
+  async findByProviderIdOrSave(dto: User): Promise<UserEntity> {
+    const { email, name, provider, providerId } = dto;
+
+    let user = await this.userRepository.findOne({ where: { providerId } });
+
+    if (!user) {
+      const u = new UserEntity();
+      u.email = email;
+      u.name = name;
+      u.provider = provider;
+      u.providerId = providerId;
+
+      user = await this.userRepository.save(u);
+    }
+
+    return user;
+  }
 }
