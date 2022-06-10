4b4df6b4fa26815775625fffeee9397a250ba4f1
young-sinf <wjdtp93@gmail.com>
2022-06-10 10:19:50 +0900

    chore: Add user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  provider: string;
+
+  @Column({ type: 'varchar' })
+  providerId: string;
+}
