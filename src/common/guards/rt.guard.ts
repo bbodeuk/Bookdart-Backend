a6f9f9e48902f7cf5ee784e25e774a09b8078e6d
young-sinf <wjdtp93@gmail.com>
2022-06-17 12:40:25 +0900

    chore: Fix guards

--- src/common/guards/rt.guard.ts
+++ src/common/guards/rt.guard.ts
@@ -2,8 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class RtGuard extends AuthGuard('jwt-refresh') {
-  constructor() {
-    super();
-  }
-}
+export class RtGuard extends AuthGuard('jwt-refresh') {}
