732511fb4abebb0629dcdfc6a9158e7a7a2dde65
young-sinf <wjdtp93@gmail.com>
2022-06-22 20:54:30 +0900

    chore: Add parameter in createGroup

---
+++ src/group/dto/create-group.dto.ts
@@ -0,0 +1,5 @@
+export class CreateGroupReq {
+  name: string;
+
+  visibility: string;
+}
--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { GroupService } from './group.service';
+import { CreateGroupReq } from './dto/create-group.dto';
 
 @Controller('groups')
 export class GroupController {
   constructor(private groupService: GroupService) {}
 
   @Post()
-  async createGroup(): Promise<void> {}
+  async createGroup(
+    @Req() req: Request,
+    @Body() dto: CreateGroupReq,
+  ): Promise<void> {}
 }
