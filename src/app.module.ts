6cc8f6d4a20fad249fa16c42504a440172af5145
young-sinf <wjdtp93@gmail.com>
2022-07-03 20:53:58 +0900

    chore: Add bookmark module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { GroupModule } from './group/group.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { GroupModule } from './group/group.module';
     TypeOrmModule.forRoot(),
     AuthModule,
     GroupModule,
+    BookmarkModule,
   ],
   controllers: [],
   providers: [ConfigService, { provide: 'APP_PIPE', useClass: ValidationPipe }],
