648f60f72f40a22869a32e04630b4ede8c79227d
young-sinf <wjdtp93@gmail.com>
2022-07-10 21:49:37 +0900

    feat: Check token for access

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 import {
   CreateBookmarkReq,
   CreateBookmarkRes,
 } from './dto/create-bookmark.dto';
 import { Success } from '../common/responses/success';
+import { AtGuard } from '../common/guards/at.guard';
 
 @Controller('bookmarks')
+@UseGuards(AtGuard)
 export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   async createBookmark(
     @Body() { groupId, link }: CreateBookmarkReq,
   ): Promise<Success<CreateBookmarkRes>> {
