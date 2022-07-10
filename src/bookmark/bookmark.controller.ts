72ae9d92e8675a38670b6228a947274e4b39f32e
young-sinf <wjdtp93@gmail.com>
2022-07-10 19:43:29 +0900

    feat: Add create bookmark

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
+import {
+  CreateBookmarkReq,
+  CreateBookmarkRes,
+} from './dto/create-bookmark.dto';
+import { Success } from '../common/responses/success';
 
 @Controller('bookmarks')
 export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
+
+  @Post()
+  async createBookmark(
+    @Body() { groupId, link }: CreateBookmarkReq,
+  ): Promise<Success<CreateBookmarkRes>> {
+    const bookmark = await this.bookmarkService.createBookmark(groupId, link);
+
+    return new Success<CreateBookmarkRes>({ bookmark });
+  }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -3,10 +3,43 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { BookmarkEntity } from './bookmark.entity';
 
+interface BookmarkMeta {
+  title: string;
+  description: string;
+  thumnail: string;
+}
+
 @Injectable()
 export class BookmarkService {
   constructor(
     @InjectRepository(BookmarkEntity)
     private bookmarkRepository: Repository<BookmarkEntity>,
   ) {}
+
+  async createBookmark(groupId: string, link: string): Promise<BookmarkEntity> {
+    // TODO: Get group by groupId, and check
+
+    const { title, description, thumnail } = await this.fromLink(link);
+
+    const bookmarkEntity = new BookmarkEntity();
+    bookmarkEntity.title = title;
+    bookmarkEntity.description = description;
+    bookmarkEntity.thumnail = thumnail;
+    bookmarkEntity.link = link;
+
+    const bookmark = await this.bookmarkRepository.save(bookmarkEntity);
+
+    return bookmark;
+  }
+
+  private async fromLink(link: string): Promise<BookmarkMeta> {
+    // TODO: get meta data from link
+    // TODO: if incorrect link, reject
+
+    const title = 'title';
+    const description = 'description';
+    const thumnail = 'thumnail';
+
+    return { title, description, thumnail };
+  }
 }
