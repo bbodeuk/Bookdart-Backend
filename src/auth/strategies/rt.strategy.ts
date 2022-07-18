03c3a3cdea913601152360fa2d3a48acdf2629c4
young-sinf <wjdtp93@gmail.com>
2022-07-18 19:00:39 +0900

    feat: Get token from cookie

--- src/auth/strategies/rt.strategy.ts
+++ src/auth/strategies/rt.strategy.ts
@@ -1,24 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload, JwtPayloadWithRT } from 'src/@types/auth';
+import getTokenFromCookies from '../../utils/get-token-from-cookies';
 
 @Injectable()
 export class JwtRtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => {
-          const token = req.cookies['refresh-token'];
-
-          if (!token) {
-            throw new UnauthorizedException();
-          }
-
-          return token;
-        },
-      ]),
+      jwtFromRequest: ExtractJwt.fromExtractors([getTokenFromCookies]),
       secretOrKey: process.env.JWT_SECRET,
       passReqToCallback: true,
     });
