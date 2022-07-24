bc16747e57c9be64039f74ce9afb37b1b1d15b7c
young-sinf <wjdtp93@gmail.com>
2022-07-24 20:12:37 +0900

    feat: Check expiration in access token

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { decode } from 'jsonwebtoken';
 import { JwtPayload } from 'src/@types/auth';
 import { User } from 'src/@types/users';
 import { UserService } from '../../user/user.service';
@@ -18,11 +19,15 @@ export class JwtAtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(req: Request, { id }: JwtPayload): Promise<User> {
-    // TODO: Check access token expiration
-    // TODO: Check refresh token and new
+    const at = req.headers.authorization.replace('Bearer ', '');
+    const now = new Date().getTime();
+
+    if ((decode(at) as JwtPayload).exp < now) {
+      // TODO: Check refresh token and new
+      throw new Error();
+    }
 
     const user = await this.userService.findById(id);
-
     return user;
   }
 }
