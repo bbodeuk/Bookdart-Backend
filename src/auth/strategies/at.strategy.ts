33952f9306ac8dec94d3292dc7fc3f749f143c97
young-sinf <wjdtp93@gmail.com>
2022-07-24 21:40:11 +0900

    feat: Get new token

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { decode } from 'jsonwebtoken';
+import { decode, verify } from 'jsonwebtoken';
 import { JwtPayload } from 'src/@types/auth';
 import { User } from 'src/@types/users';
 import { UserService } from '../../user/user.service';
+import { AuthService } from '../auth.service';
+import { Token } from '../../@types/auth';
 
 @Injectable()
 export class JwtAtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
@@ -18,16 +23,39 @@ export class JwtAtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(req: Request, { id }: JwtPayload): Promise<User> {
+  async validate(req: Request, payload: JwtPayload): Promise<User> {
+    const { id } = payload;
     const at = req.headers.authorization.replace('Bearer ', '');
     const now = new Date().getTime();
+    const user = await this.userService.findById(id);
 
-    if ((decode(at) as JwtPayload).exp < now) {
-      // TODO: Check refresh token and new
-      throw new Error();
+    if ((decode(at) as JwtPayload).exp > now) {
+      return user;
     }
 
-    const user = await this.userService.findById(id);
+    const rt = req.cookies['refresh-token'];
+
+    this.checkRefreshToken(rt);
+    await this.authService.checkHashedRefreshToken(user.hashedRefreshToken, rt);
+
+    const token = this.getNewToken(payload);
+
+    // TODO: Update refresh token in user
+    // TODO: Change token in cookie
+
     return user;
+  }
+
+  private checkRefreshToken(refreshToken: string) {
+    try {
+      verify(refreshToken, process.env.JWT_SECRET);
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private getNewToken(payload: JwtPayload): Token {
+    const token = this.authService.login(payload);
+    return token;
   }
 }
--- src/auth/strategies/rt.strategy.ts
+++ src/auth/strategies/rt.strategy.ts
@@ -3,13 +3,12 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload, JwtPayloadWithRT } from 'src/@types/auth';
-import getTokenFromCookies from '../../utils/get-token-from-cookies';
 
 @Injectable()
 export class JwtRtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([getTokenFromCookies]),
+      jwtFromRequest: ExtractJwt.fromExtractors([]),
       secretOrKey: process.env.JWT_SECRET,
       passReqToCallback: true,
     });
