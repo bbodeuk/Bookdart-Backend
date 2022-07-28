a5de2feff8ca6a1d4326bd6741a258c57eedd341
young-sinf <wjdtp93@gmail.com>
2022-07-28 11:57:14 +0900

    feat: Add created and updated in group

--- src/group/group.entity.ts
+++ src/group/group.entity.ts
@@ -1,9 +1,11 @@
 import {
   Column,
+  CreateDateColumn,
   Entity,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { UserEntity } from '../user/user.entity';
 import { BookmarkEntity } from '../bookmark/bookmark.entity';
@@ -24,4 +26,10 @@ export class GroupEntity {
 
   @OneToMany(() => BookmarkEntity, (bookmark) => bookmark.group)
   bookmarks: BookmarkEntity[];
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
 }
