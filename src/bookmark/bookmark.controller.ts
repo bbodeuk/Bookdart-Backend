dd3cf40308a7896384601d367bb4b71d96338b29
Young <43630508+young-sinf@users.noreply.github.com>
2022-07-30 10:36:56 +0900

    feat: Add API for removing bookmark (#22)
    * feat: Add delete bookmark
    
    * feat: Check owner when delete bookmark
    
    * feat: Add validator when delete bookmark
    
    * fix: Add path for delete bookmark
    
    * chore: Find group with user
    
    * feat: Add exception when don't have bookmark

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,11 +1,15 @@
 import {
   Body,
   Controller,
+  Delete,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { BookmarkService } from './bookmark.service';
 import {
   CreateBookmarkReq,
@@ -13,6 +17,8 @@ import {
 } from './dto/create-bookmark.dto';
 import { Success } from '../common/responses/success';
 import { AtGuard } from '../common/guards/at.guard';
+import DeleteBookmarkReq from './dto/delete-bookmark.dto';
+import { User } from '../@types/users';
 
 @Controller('bookmarks')
 @UseGuards(AtGuard)
@@ -30,5 +36,19 @@ export class BookmarkController {
     return new Success<CreateBookmarkRes>({
       bookmark: { title, description, image, link },
     });
+  }
+
+  @Delete(':bookmarkId')
+  @HttpCode(HttpStatus.OK)
+  async deleteBookmark(
+    @Req() req: Request,
+    @Param('bookmarkId') bookmarkId: string,
+    @Body() { groupId }: DeleteBookmarkReq,
+  ): Promise<Success<null>> {
+    const user = req.user as User;
+
+    await this.bookmarkService.deleteBookmark(user, bookmarkId, groupId);
+
+    return new Success(null);
   }
 }
